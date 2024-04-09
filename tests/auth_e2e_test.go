31a8305ab3150e707c66dcba1d2389993358d670
LailaCharaoui <mats2@live.fr>
2024-04-09 07:34:39 +0200

    feat(test): add some tests

--- tests/auth_e2e_test.go
+++ tests/auth_e2e_test.go
@@ -3,19 +3,43 @@ package handlers_test
 import (
 	"authentication-api/handlers"
 	"authentication-api/models"
+	"authentication-api/services"
 	"bytes"
 	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func TestLoginWithValidCredentials(t *testing.T) {
-	router := gin.Default()
-	router.POST("/login", handlers.Login)
+func initApp() *gin.Engine {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+
+	//@TODO add default user if not exists with username: user, password: password
+	var user models.User
+	db.First(&user, "username = ?", "user")
+	if user.ID == 0 {
+		user = models.User{Username: "user", Password: "password"}
+		db.Create(&user)
+	}
+	authService := services.NewAuthService(db)
+	handler := handlers.AuthHandler{AuthService: authService}
+	router := gin.Default()
+	router.POST("/login", handler.Login)
+	router.POST("/register", handlers.Register)
+	return router
+
+}
+
+func TestLoginWithValidCredentials(t *testing.T) {
+	router := initApp()
 	user := models.User{Username: "user", Password: "password"}
 	body, _ := json.Marshal(user)
 	req, _ := http.NewRequest(http.MethodPost, "/login", bytes.NewBuffer(body))
@@ -28,9 +52,7 @@ func TestLoginWithValidCredentials(t *testing.T) {
 }
 
 func TestLoginWithInvalidCredentials(t *testing.T) {
-	router := gin.Default()
-	router.POST("/login", handlers.Login)
-
+	router := initApp()
 	user := models.User{Username: "wrong", Password: "wrong"}
 	body, _ := json.Marshal(user)
 	req, _ := http.NewRequest(http.MethodPost, "/login", bytes.NewBuffer(body))
