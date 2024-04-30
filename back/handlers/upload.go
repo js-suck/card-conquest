03446e17fc7c23744a9f001f95ca8639eb24cd9c
LailaCharaoui <mats2@live.fr>
2024-04-30 08:11:22 +0200

    feat(media): add media upload feature

---
+++ back/handlers/upload.go
@@ -0,0 +1,37 @@
+package handlers
+
+import (
+	"authentication-api/services"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type UploadHandler struct {
+	FileService *services.FileService
+}
+
+func NewUploadHandler(f *services.FileService) *UploadHandler {
+	return &UploadHandler{FileService: f}
+}
+
+// UploadImage  godoc
+// @Summary Upload an image
+// @Description Upload an image
+// @Tags Upload
+// @Accept mpfd
+// @Produce json
+// @Param image formData file true "Image file"
+// @Success 200 {object} string
+// @Failure 400 {object} string
+// @Router /images [post]
+func (u *UploadHandler) UploadImage(c *gin.Context) {
+	file, err := c.FormFile("image")
+
+	_, filePath, errUpload := u.FileService.UploadMedia(file)
+	if err != nil {
+		c.JSON(errUpload.Code(), errUpload.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Image uploaded successfully!", "path": filePath})
+}
