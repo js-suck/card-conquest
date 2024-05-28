d8b2dab6b25fb0568ba4d5c5e2e5a6e2d47937e2
LailaCharaoui <mats2@live.fr>
2024-05-28 10:38:04 +0200

    feat(tournament): fix wrong json return

--- back/handlers/tournament.go
+++ back/handlers/tournament.go
@@ -4,9 +4,7 @@ import (
 	"authentication-api/errors"
 	"authentication-api/models"
 	"authentication-api/services"
-	"encoding/json"
 	"github.com/gin-gonic/gin"
-	"log"
 	"net/http"
 	"strconv"
 )
@@ -19,6 +17,33 @@ type TournamentHandler struct {
 
 func NewTournamentHandler(tournamentService *services.TournamentService, fileService *services.FileService, matchService *services.MatchService) *TournamentHandler {
 	return &TournamentHandler{TounamentService: tournamentService, FileService: fileService, MatchService: matchService}
+}
+
+func (h *TournamentHandler) parseFilterParams(c *gin.Context) services.FilterParams {
+	TournamentID := c.Query("TournamentID")
+	UserID := c.Query("UserID")
+	GameID := c.Query("GameID")
+
+	filterParams := services.FilterParams{
+		Fields: map[string]interface{}{},
+		Sort:   []string{},
+	}
+
+	if TournamentID != "" {
+		filterParams.Fields["TournamentID"] = TournamentID
+
+	}
+
+	if UserID != "" {
+		filterParams.Fields["UserID"] = UserID
+
+	}
+
+	if GameID != "" {
+		filterParams.Fields["GameID"] = GameID
+	}
+
+	return filterParams
 }
 
 // CreateTournament godoc
@@ -194,21 +219,15 @@ func (h *TournamentHandler) GetTournaments(c *gin.Context) {
 			recentTournaments[i] = tournament
 		}
 
-		jsonResponse, err := json.Marshal(gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"allTournaments":    formattedTournaments,
 			"recentTournaments": recentTournaments,
 		})
-		if err != nil {
-			log.Printf("Error marshalling JSON: %v", err)
-		} else {
-			log.Printf("JSON Response: %s", jsonResponse)
-		}
 
-		c.JSON(http.StatusOK, jsonResponse)
+	} else {
 
+		c.JSON(http.StatusOK, formattedTournaments)
 	}
-
-	c.JSON(http.StatusOK, formattedTournaments)
 }
 
 // RegisterUser godoc
@@ -335,4 +354,73 @@ func (h *TournamentHandler) StartTournament(context *gin.Context) {
 	}
 
 	context.JSON(http.StatusOK, "Tournament started successfully")
+}
+
+// GetTournamentMatches godoc
+// @Summary Get all matches of a tournament
+// @Description Get all matches of a tournament
+// @Tags tournament
+// @Accept json
+// @Produce json
+// @Param id path int true "Tournament ID"
+// @Success 200 {object} string
+// @Failure 400 {object} errors.ErrorResponse
+// @Failure 500 {object} errors.ErrorResponse
+// @Security BearerAuth
+// @Param Authorization header string true "Insert your access token" default(Bearer <Add access token here>)
+// @Router /tournaments/{id}/matches [get]
+func (h *TournamentHandler) GetTournamentMatches(context *gin.Context) {
+	tournamentID, err := strconv.Atoi(context.Param("id"))
+	if err != nil {
+		context.JSON(http.StatusBadRequest, errors.NewBadRequestError("Invalid ID", err).ToGinH())
+		return
+	}
+
+	matches, errService := h.MatchService.GetTournamentMatches(uint(tournamentID))
+	if errService != nil {
+		context.JSON(errService.Code(), errService)
+		return
+	}
+
+	context.JSON(http.StatusOK, matches)
+}
+
+// GetRankings godoc
+// @Summary Get the ranking of a tournament
+// @Description Get the ranking of a tournament
+// @Tags tournament
+// @Accept json
+// @Produce json
+// @Param TournamentID query int false "Tournament ID"
+// @Param GameID query int false "Game ID"
+// @Param UserID query int false "User ID"
+// @Success 200 {object} string
+// @Failure 400 {object} errors.ErrorResponse
+// @Failure 500 {object} errors.ErrorResponse
+// @Security BearerAuth
+// @Param Authorization header string true "Insert your access token" default(Bearer <Add access token here>)
+// @Router /tournaments/rankings [get]
+func (h *TournamentHandler) GetRankings(c *gin.Context) {
+
+	if c.Query("TournamentID") != "" || c.Query("GameID") != "" || c.Query("UserID") != "" {
+
+		filterParams := h.parseFilterParams(c)
+
+		ranking, errService := h.TounamentService.CalculateRanking(&filterParams)
+		if errService != nil {
+			c.JSON(errService.Code(), errService)
+			return
+		}
+		c.JSON(http.StatusOK, ranking)
+		return
+
+	}
+
+	ranking, errService := h.TounamentService.CalculateGlobalRanking()
+	if errService != nil {
+		c.JSON(errService.Code(), errService)
+		return
+	}
+
+	c.JSON(http.StatusOK, ranking)
 }
