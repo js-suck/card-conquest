e9eade6fb96e7d04be51f575cf294f0010cf19b5
Vivian Ruhlmann <vivian.fr@free.fr>
2024-07-17 12:48:33 +0200

    :bug:(Tournament): Fix api tournament

--- back/models/tournament.go
+++ back/models/tournament.go
@@ -42,23 +42,29 @@ type CreateTournamentPayload struct {
 type Tournament struct {
 	BaseModel
 	MediaModel
+	ID          uint             `json:"id"`
 	Name        string           `json:"name" validate:"required"`
 	Description string           `json:"description" validate:"required"`
 	Location    string           `json:"location"`
 	UserID      uint             `json:"organizer_id" `
-	User        *User            `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	User        *User            `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	GameID      uint             `json:"game_id" validate:"required"`
-	Game        Game             `gorm:"foreignKey:GameID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	Game        Game             `gorm:"foreignKey:GameID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	StartDate   string           `json:"start_date" validate:"required"`
 	EndDate     string           `json:"end_date" validate:"required"`
 	Status      string           `json:"status" gorm:"default:opened"`
-	Users       []*User          `gorm:"many2many:user_tournaments;"`
+	Users       []*User          `gorm:"many2many:user_tournaments;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	Tags        []*Tag           `json:"tags" gorm:"many2many:tag_tournaments;"`
 	Rounds      int              `json:"rounds" validate:"required"`
 	MaxPlayers  int              `json:"maxPlayers" validate:"required" example:"32"`
-	Steps       []TournamentStep `json:"tournament_steps" gorm:"foreignKey:TournamentID"`
+	Steps       []TournamentStep `json:"tournament_steps" gorm:"foreignKey:TournamentID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	Longitude   float64          `json:"longitude"`
 	Latitude    float64          `json:"latitude"`
+	Subscribers []User           `gorm:"many2many:tournament_subscribers;"`
+}
+
+func (t Tournament) New() IModel {
+	return &Tournament{}
 }
 
 type TournamentRead struct {
@@ -164,25 +170,8 @@ func (t Tournament) ToRead() TournamentRead {
 
 }
 
-func (m Tournament) IsOwner(userID uint) bool {
-	return true
-}
-
-type UpdateTournamentPayload struct {
-	Name        string  `json:"name" example:"Tournament 1" form:"name"`
-	Description string  `json:"description" example:"Tournament 1 description" form:"description"`
-	Location    string  `json:"location" example:"New York" form:"location"`
-	UserID      uint    `json:"organizer_id" example:"1" form:"organizer_id"`
-	GameID      uint    `json:"game_id" example:"1" form:"game_id"`
-	StartDate   string  `json:"start_date" json:"start_date" form:"start_date"`
-	EndDate     string  `json:"end_date" json:"end_date" form:"end_date"`
-	Status      string  `json:"status" form:"status"`
-	Rounds      int     `json:"rounds" example:"3" form:"rounds"`
-	TagsIDs     []uint  `json:"tags_idss" form:"tags_ids"`
-	MaxPlayers  int     `json:"max_players" example:"32" form:"max_players"`
-	Longitude   float64 `json:"longitude" form:"longitude"`
-	Latitude    float64 `json:"latitude" form:"latitude"`
-	Image       []byte  `gorm:"type:longblob" json:"-"`
+func (t Tournament) IsOwner(userID uint) bool {
+	return t.UserID == userID
 }
 
 type UpdateTournamentPayload struct {
